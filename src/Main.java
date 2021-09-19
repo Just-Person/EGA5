c567fd9cdc059d7df687a6d0aa6466a0446a4bbc
Just-Person <ilja.guryanow3636@yandex.ru>
2021-09-19 12:23:00 +0300

    Project without Grey

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args)
     {
         Random rnd = new Random();
-        int randomstart = Math.abs(rnd.nextInt()) % mypow(2,length);
+        int randomstart = Math.abs(rnd.nextInt()) % (mypow(2,length) - 1) + 1;
         int randommethod= Math.abs(rnd.nextInt()) % 3;
         int i = 0;
         Double max = 0.0;
@@ -16,8 +16,8 @@ public class Main {
         System.out.print("Введите количество шагов у программы: ");
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        MyString myString = new MyString(length,randomstart,1);
-        myString.printhash();
+        MyString myString = new MyString(length,randomstart);
+        myString.print();
         maxX = myString.dectotwo(randomstart,length);
         max = 5*Math.sin(randomstart) + Math.log(randomstart);
         while (i<n)
@@ -29,16 +29,16 @@ public class Main {
             if (randommethod == 0)
             {
                 System.out.println("Метод поиска Монте-Карло: ");
-                myString = new MyString(length,randomstart,1);
+                myString = new MyString(length,randomstart);
                 myrandom(myString,n);
             } else if (randommethod == 1)
             {
                 System.out.println("Метод поиска в глубину: ");
-                myString = new MyString(length,randomstart,1);
+                myString = new MyString(length,randomstart);
                 dfs(myString,n);
             } else
             {   System.out.println("Метод поиска в глубину: ");
-                myString = new MyString(length,randomstart,1);
+                myString = new MyString(length,randomstart);
                 bfs(myString,n);
             }
             if (max < myString.getMax()){
@@ -46,7 +46,7 @@ public class Main {
                 max = myString.getMax();
                 maxX =myString.getMaxX();
                 System.out.println("Новый итоговый максимум - "+ maxX + " значение - " + df2.format(max));
-                randomstart = Math.abs(rnd.nextInt()) % mypow(2,length);
+                randomstart = Math.abs(rnd.nextInt()) % (mypow(2,length) - 1) + 1;;
             } else System.out.println("Итоговый максимум не поменялся");
         }
         System.out.println("Найденный итоговый максимум - " + maxX + " значение - " + df2.format(max));
@@ -56,7 +56,7 @@ static void dfs(MyString myString, int n)
 {
     System.out.println("Точка входа - "+ myString.getMaxX() + "======"
             + df2.format(myString.geti(myString.getMaxX())));
-    myString.myremovehash(myString.getMaxX());
+    myString.myremove(myString.getMaxX());
     int i = 0;
     String buffer = "";
     while (i < n)
@@ -77,7 +77,7 @@ static void dfs(MyString myString, int n)
                 System.out.println("Новый максимум - " + myString.getMaxX()
                         + ", значение - " + df2.format(myString.getMax()));
             } else System.out.println("Максимум не поменялся");
-            myString.myremovehash(buffer);
+            myString.myremove(buffer);
         }
         else {
             i = n + 1;
@@ -91,7 +91,7 @@ static void bfs(MyString myString, int n)
 {
         System.out.println("Точка входа - "+ myString.getMaxX() + "======"
                 + df2.format(myString.geti(myString.getMaxX())));
-        myString.myremovehash(myString.getMaxX());
+        myString.myremove(myString.getMaxX());
         int i = 0;
         String buffer = "";
         while (i < n)
@@ -124,7 +124,7 @@ static void myrandom(MyString myString, int n)
         int randomstart = 0;
         System.out.println("Точка входа - "+ myString.getMaxX() + "======"
                 + df2.format(myString.geti(myString.getMaxX())));
-        myString.myremovehash(myString.getMaxX());
+        myString.myremove(myString.getMaxX());
         for (int i = 0; i<n; i++)
         {
             randomstart = Math.abs(rnd.nextInt()) % mypow(2,length);
@@ -144,62 +144,12 @@ static void myrandom(MyString myString, int n)
                             + myString.getMaxX() +", его значение - "
                             + df2.format(myString.getMax()));
                 } else System.out.println("Максимум не поменялся");
-                myString.myremovehash(bufstr);
+                myString.myremove(bufstr);
             } else System.out.println("Мы уже посещали эту точку!");
         }
         System.out.println("Максимальная строчка - " + myString.getMaxX()
                 + ", её значение - " + df2.format(myString.getMax()));
     }
-static void grey (MyString myString, int n)
-{
-    System.out.println("Точка входа - "+ myString.getMaxX()
-            + "======" + df2.format(myString.geti(myString.getMaxX())));
-    myString.printgrey();
-    myString.myremovegrey(myString.getMaxX());
-    myString.getHashMap().clear();
-    myString.getHashMap().put(myString.getMaxX(),myString.getMax());
-    System.out.println("Первоначальный максимум - "+ myString.getMaxX()
-            + ", значение -  " + df2.format(myString.getMax()));
-    int i = 0;
-    String buffer = "";
-    while (i < n)
-    {
-        System.out.println("Номер шага - "+ i);
-        System.out.println("Текущий max - "+ myString.getMaxX()
-                + ", значение - " + df2.format(myString.getMax()));
-        myString.printlocal(myString.getMaxX());
-        buffer = myString.getIndexGrey();
-        if (buffer!=null)
-        {
-            System.out.println("Мы нашли соседнюю строчку - " + buffer);
-            i++;
-            if (myString.geti(buffer) > myString.getMax())
-            {
-                myString.setMax(myString.geti(buffer));
-                myString.setMaxX(buffer);
-                System.out.println("Новый максимум - " + myString.getMaxX()
-                        + ", значение - " + df2.format(myString.getMax()));
-                myString.getHashMap().put(myString.getMaxX(),myString.getMax());
-            } else System.out.println("Максимум не поменялся");
-            myString.myremovegrey(buffer);
-        }
-        else {
-            i = n + 1;
-            System.out.println("Больше соседей нет!");
-        }
-    }
-    System.out.println("Максимальная строчка - " + myString.getMaxX()
-            + ", её значение - " + df2.format(myString.getMax()));
-    System.out.print("Наш путь - ");
-    myString.printhash();
-    myString.myremovehash(myString.getMaxX());
-    //myString.getHashMap().put(myString.dectotwo(randomstart, 7), Math.sin(0));
-    System.out.println();
-    System.out.print("После перевода в бинарный код: ");
-    System.out.println(myString.greyreverse(myString.getMaxX())
-            + "======"+df2.format(myString.getMax()));
-    myString.setMaxX(myString.greyreverse(myString.getMaxX()));
-}
 static int mypow(int first, int second)
 {
         int buffer = first;
--- src/MyString.java
+++ src/MyString.java
@@ -3,71 +3,23 @@ import java.util.HashMap;
 
 public class MyString {
     private HashMap<String, Double> hashMap = new HashMap<>();
-    private HashMap<String, Double> greyMap = new HashMap<>();
     private int startposition = 0;
     private int length = 0;
     private Double max = 0.0;
     private String maxX = "";
-    private int start = 0;
-    private int how = 0;
+    private int start = 1;
     private static DecimalFormat df2 = new DecimalFormat("#.##");
 
-
-
-    //if how == 0 - grey, another - simply
-    MyString (int length, int startposition, int how)
+    MyString (int length, int startposition)
     {
-        this.how = how;
         this.length = length;
         for (int i = this.start; i<mypow(2,length); i++)
         {
             this.hashMap.put(dectotwo(i,length),5*Math.sin(i) + Math.log(i));
         }
-        for (int i = this.start; i<mypow(2,length);i++)
-        {
-            this.greyMap.put(grey(dectotwo(i,length),1),5*Math.sin(i) + Math.log(i));
-        }
         this.startposition = startposition;
-        if (how == 0) {
-            this.maxX = grey(dectotwo(startposition, length), 1);
-            this.max = this.greyMap.get(maxX);
-        } else {
             this.maxX = dectotwo(startposition, length);
             this.max = this.hashMap.get(maxX);
-        }
-    }
-    public String plus (String s1, String s2)
-    {
-        String s = "";
-        for (int i = 0; i<s1.length();i++)
-        {
-            if ((s1.charAt(i)=='1' && s2.charAt(i)=='0')
-                    || (s1.charAt(i)=='0' && s2.charAt(i)=='1')) s +="1";
-            else s +="0";
-        }
-        return s;
-    }
-
-    public String grey (String s1, int index)
-    {
-        String s = "";
-        for (int i = 0; i<index; i++)
-            s +="0";
-        s+=s1.substring(0,s1.length()-index);
-        s = plus(s,s1);
-        return s;
-    }
-    public String getIndexGrey ()
-    {
-        String buffer = "";
-        for (int i = 0; i<this.length; i++)
-        {
-            buffer = changeIndexString(maxX,i);
-            if (getigrey(buffer)!=null) {
-                return buffer;
-            }
-        }
-        return null;
     }
     public String getIndexHash ()
     {
@@ -106,7 +58,7 @@ public String getIndexBFS()
         numbuf = geti(buffer);
         if (geti(buffer)!=null) {
             hashbuf.put(buffer,numbuf);
-            myremovehash(buffer);
+            myremove(buffer);
         }
     }
     numbuf = -1.0;
@@ -183,78 +135,33 @@ public String getIndexBFS()
             return first;
         }
     }
-    public void printlocalgrey(String num)
-    {
-        System.out.print("Текущая окрестность: ");
-        String buffer = "";
-        int k = 0;
-        for (int i = 0; i<this.length; i++)
-        {
-            buffer = changeIndexString(num,i);
-            if (geti(buffer)!=null) {
-                System.out.print(buffer+"  ");
-                k = 1;
-            }
-        }
-        if (k==1)
-            System.out.println();
-    }
-    public void printlocalhash(String num)
-    {
-        System.out.print("Текущая окрестность: ");
-        String buffer = "";
-        int k = 0;
-        for (int i = 0; i<this.length; i++)
-        {
-            buffer = changeIndexString(num,i);
-            if (geti(buffer)!=null) {
-                System.out.print(buffer+"  ");
-                k = 1;
-            }
-        }
-        if (k==1)
-            System.out.println();
-    }
     public void printlocal(String num)
     {
-        if (this.how == 0) printlocalgrey(num);
-        else printlocalhash(num);
+        System.out.print("Текущая окрестность: ");
+        String buffer = "";
+        int k = 0;
+        for (int i = 0; i<this.length; i++)
+        {
+            buffer = changeIndexString(num,i);
+            if (geti(buffer)!=null) {
+                System.out.print(buffer+"  ");
+                k = 1;
+            }
+        }
+        if (k==1)
+            System.out.println();
     }
+
     public Double geti(String num)
-    {
-        if (this.how == 0)
-            return getigrey(num);
-        else return getihash(num);
-    }
-    public Double getigrey(String num)
-    {
-        if (greyMap.get(num)!=null)
-            return greyMap.get(num);
-        else return null;
-    }
-    public Double getihash(String num)
     {
         if (hashMap.get(num)!=null)
             return hashMap.get(num);
         else return null;
     }
 
-    public HashMap<String, Double> getGreyMap() {
-        return greyMap;
-    }
 
-    public void setGreyMap(HashMap<String, Double> greyMap) {
-        this.greyMap = greyMap;
-    }
 
-    public void myremovegrey (String del)
-    {
-        if (greyMap.get(del) != null)
-        {
-            greyMap.remove(del);
-        }
-    }
-    public void myremovehash (String del)
+    public void myremove (String del)
     {
         if (hashMap.get(del) != null)
         {
@@ -266,62 +173,14 @@ public String getIndexBFS()
     {
         return hashMap.size();
     }
-    public void printgrey() {
-        for (int i = this.start; i < greyMap.size(); i++) {
-            if (greyMap.get(grey(dectotwo(i,7),1)) != null)
-                System.out.println(grey(dectotwo(i,length),1) + "======"
-                        + df2.format(greyMap.get(grey(dectotwo(i,length),1))));
-        }
-    }
-    public void printhashgrey() {
-        double min = 1000.0;
-        double buffer = 0.0;
-        String index = "";
-        for (int j = this.start; j < mypow(2, this.length); j++) {
-            min = 1000.0;
-        for (int i = this.start; i < mypow(2, this.length); i++) {
-            if (hashMap.get(grey(dectotwo(i, this.length), 1)) != null
-                    && hashMap.get(grey(dectotwo(i, this.length), 1)) < min) {
-                min = hashMap.get(grey(dectotwo(i, this.length), 1));
-                index = grey(dectotwo(i, this.length), 1);
 
-            }
-        }
-            if (hashMap.get(index) != null) {
-                System.out.print(index + "======"
-                        + df2.format(min) + " ");
-                hashMap.remove(index);
-            }
-    }
-    }
-    public void printhashnotgrey()
-    {
+    public void print(){
         for (int i = this.start; i < mypow(2, this.length); i++) {
             if (hashMap.get(dectotwo(i, this.length)) != null) {
                 System.out.println(dectotwo(i, this.length) + "======"
                         + df2.format(hashMap.get(dectotwo(i, this.length))));
             }
         }
-    }
-    public void printhash(){
-        if (this.how==0) printhashgrey();
-        else printhashnotgrey();
-    }
-    public String greyreverse(String str)
-    {
-        HashMap <Integer,String> buffer = new HashMap<>();
-        buffer.put(0,str);
-        for (int i = 1; i<this.length; i++)
-        {
-            str = plus(str,grey(str,1));
-            buffer.put(i,str);
-        }
-        str = buffer.get(0);
-        for (int i = 1; i<this.length; i++)
-        {
-            str = plus(str,buffer.get(i));
-        }
-        return str;
     }
     public HashMap<String, Double> getHashMap() {
         return hashMap;
@@ -360,12 +219,5 @@ public String getIndexBFS()
 
     public void setLength(int length) {
         this.length = length;
-    }
-    public int getHow() {
-        return how;
-    }
-
-    public void setHow(int how) {
-        this.how = how;
     }
 }
